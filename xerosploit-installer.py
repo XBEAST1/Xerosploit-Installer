54d230c99cc2648907b08f1da3f84cdb2f17d54b
XBEAST1 <81472131+XBEAST1@users.noreply.github.com>
2023-07-09 20:06:02 +0500

    V1

---
+++ xerosploit-installer.py
@@ -0,0 +1,33 @@
+import os
+
+print ("""
+   _  __                      __     _ __    ____         __       ____       
+  | |/_/__ _______  ___ ___  / /__  (_) /_  /  _/__  ___ / /____ _/ / /__ ____
+ _>  </ -_) __/ _ \(_-</ _ \/ / _ \/ / __/ _/ // _ \(_-</ __/ _ `/ / / -_) __/
+/_/|_|\__/_/  \___/___/ .__/_/\___/_/\__/ /___/_//_/___/\__/\_,_/_/_/\__/_/   
+                     /_/                                                      
+	by XBEAST ~ Github Link: https://github.com/XBEAST1
+""")
+
+os.system("sudo apt update -y")
+os.system("sudo apt install nmap -y")
+os.system("sudo apt install bettercap -y")
+os.system("sudo apt install hping3 -y")
+os.system("sudo apt install build-essential -y")
+os.system("sudo apt install ruby-dev -y")
+os.system("sudo apt install libpcap-dev -y")
+os.system("sudo apt install libgmp3-dev -y")
+os.system("sudo apt install python3-pip -y")
+os.system("sudo pip install --upgrade pip")
+os.system("sudo pip3 install --upgrade pip")
+os.system("sudo pip install terminaltables")
+os.system("sudo pip3 install terminaltables")
+os.system("sudo pip install tabulate")
+os.system("sudo pip3 install tabulate")
+os.system("sudo gem install sorted_set")
+os.system("sudo gem update xettercap")
+os.system("sudo gem install packetfu -v 1.1.11")
+os.system("sudo git clone https://github.com/LionSec/xerosploit")
+os.system("chmod 777 xerosploit")
+os.chdir("xerosploit")
+os.system("python3 install.py")
